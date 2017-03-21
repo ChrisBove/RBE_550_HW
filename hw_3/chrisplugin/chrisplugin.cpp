7bff3aec551d35f525ecfc120764e7e0d0bb541f
Chris Bove <bove.3123@gmail.com>
2017-03-21 11:43:38 -0400

    untested implementation of node tree and nearest neighbors

--- hw_3/chrisplugin/chrisplugin.cpp
+++ hw_3/chrisplugin/chrisplugin.cpp
@@ -1,6 +1,9 @@
 #include <openrave/plugin.h>
 #include <boost/bind.hpp>
 using namespace OpenRAVE;
+#include <algorithm>
+#include <math.h>
+#include <vector>
 
 #include "chrisplugin.hpp"
 
@@ -58,3 +61,81 @@ std::vector<float> RRTNode::getConfiguration(){
     return _configuration;
 }
 
+void RRTNode::setParent(RRTNode* node){
+    _parent = node;
+}
+
+void RRTNode::setConfiguration(std::vector<float> configuration){
+    _configuration = configuration;
+}
+
+NodeTree::NodeTree(){
+
+}
+
+void NodeTree::addNode(RRTNode* node){
+    _nodes.push_back(node);
+}
+
+void NodeTree::deleteNode(unsigned index){
+    _nodes.erase(_nodes.begin()+index);
+}
+
+RRTNode* NodeTree::getNode(unsigned index){
+    return _nodes[index];
+}
+
+std::vector<RRTNode*> NodeTree::getPath(unsigned index){
+    std::vector<RRTNode*> path;
+    RRTNode* currentNode = _nodes[index];
+
+    path.push_back(currentNode);
+
+    while(currentNode->getParent() != NULL){
+        currentNode = currentNode->getParent();
+        path.push_back(currentNode);
+    }
+
+    std::reverse(path.begin(), path.end());
+
+    return path;
+}
+
+float HeapableRRTNode::getCost(){
+	return _cost;
+}
+
+RRTNode* HeapableRRTNode::getRRTNode(){
+	return _node;
+}
+
+// from http://stackoverflow.com/questions/14016921/comparator-for-min-heap-in-c
+struct costComparitor{
+  bool operator()(const HeapableRRTNode& a,const HeapableRRTNode& b) const{
+    return a._cost > b._cost;
+  }
+};
+
+//naive method - search all nodes for closest weighted neighbor
+RRTNode* NodeTree::nearestNeighbor(std::vector<float> configuration, std::vector<float> weights){
+    std::vector<HeapableRRTNode> neighbors;
+    neighbors.reserve(_nodes.size()); // preallocate number of elements
+
+    // compute distances from this configuration to the neighbors we have
+    for (RRTNode* node : _nodes){
+    	neighbors.emplace_back(node, weightedEuclidDistance(node->getConfiguration(),configuration,weights));
+    }
+    // heap them so that the shortest is at the top
+    std::make_heap(neighbors.begin(), neighbors.end(), costComparitor());
+    // pick the one at the top
+    return neighbors.front().getRRTNode();
+    
+}
+
+float NodeTree::weightedEuclidDistance(std::vector<float> configuration1, std::vector<float> configuration2, std::vector<float> weights){
+    float sum = 0;
+    for(unsigned i = 0; i < configuration1.size(); i++){
+        sum += pow(weights[i]*(configuration1[i] - configuration2[i]),2);
+    }
+    return sqrt(sum);
+}
--- hw_3/chrisplugin/chrisplugin.hpp
+++ hw_3/chrisplugin/chrisplugin.hpp
@@ -8,7 +8,9 @@ class RRTNode
 public:
 	RRTNode(std::vector<float> configuration, RRTNode* parent);
 	RRTNode* getParent();
+	void setParent(RRTNode* node);
 	std::vector<float> getConfiguration();
+	void setConfiguration(std::vector<float> configuration);
 
 
 private:
@@ -16,16 +18,33 @@ private:
 	RRTNode* _parent;
 };
 
+class HeapableRRTNode{
+public:
+	HeapableRRTNode(RRTNode* node, float cost);
+	float getCost();
+	RRTNode* getRRTNode();
+	float _cost;
+private:
+	RRTNode* _node;
+
+};
+
+// make a struct for doing compares
+
 class NodeTree
 {
 public:
 	NodeTree();
-	bool addNode(RRTNode* node);
-	bool deleteNode(RRTNode* node);
-	RRTNode getNode(int index);
-	std::vector<RRTNode*> getPath(int index);
+	void addNode(RRTNode* node);
+	void deleteNode(unsigned node);
+	RRTNode* getNode(unsigned index);
+	std::vector<RRTNode*> getPath(unsigned index);
+
+	RRTNode* nearestNeighbor(std::vector<float> configuration, std::vector<float> weights);
 
 private:
 	std::vector<RRTNode*> _nodes;
+
+	float weightedEuclidDistance(std::vector<float> configuration1, std::vector<float> configuration2, std::vector<float> weights);
 
 };
